5b69e6511a8dba745b7b0bc17cb0c5e2bb44cded
youlingsi <youlingsi@hotmail.com>
2018-04-30 22:51:30 -0400

    Initial commit

---
+++ Assets/_Script/Character.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Character : MonoBehaviour {
+    public TextAsset data;
+    private CharaInfo cInfo;
+  
+
+    void Start()
+    {
+        cInfo = JsonUtility.FromJson<CharaInfo>(data.text);
+        print(cInfo.charaName);
+        print(cInfo.Descrption);
+    }
+
+
+
+}
+
+[System.Serializable]
+public class CharaInfo
+{
+    public string charaType;
+    public string charaName;
+    public int Constitution;
+    public int Intellegence;
+    public int Charisma;
+    public int Observation;
+    public string Skill1;
+    public string Skill2;
+    public string Descrption;
+}
